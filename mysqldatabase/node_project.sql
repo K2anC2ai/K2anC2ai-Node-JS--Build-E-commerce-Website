b604e5bd274def887d3acd91597a562e8dd3a7ca
K2anC2ai <khanchai.k2@gmail.com>
2024-10-27 11:56:20 +0700

    update database

--- mysqldatabase/node_project.sql
+++ mysqldatabase/node_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2024 at 04:34 AM
+-- Generation Time: Oct 27, 2024 at 05:55 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL,
+  `cost` decimal(8,2) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  `product_ids` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `cost`, `name`, `email`, `status`, `city`, `address`, `phone`, `date`, `product_ids`) VALUES
+(1, 1400.00, 'asd', 'asd@e', 'paid', 'asd', 'asd', 'asd', '2024-10-25 20:06:51', ',1'),
+(2, 8400.00, 'khanchai khamnong', 'headfoottv2545@gmail.com', 'paid', 'asdasd', 'asda', '0151154', '2024-10-25 23:07:18', ',1'),
+(3, 298.00, 'khanchai khamnong', 'headfoottv2545@gmail.com', 'paid', 'chon buri', '169 ถนน ลงหาดบางแสน ตำบลแสนสุข อำเภอเมืองชลบุรี ชลบุรี 20131', '3936242491', '2024-10-27 11:27:24', ',1,2'),
+(4, 99.00, 'khanchai khamnong', 'headfoottv2545@gmail.com', 'paid', 'chon buri', 'klaeng', '3936242491', '2024-10-27 11:51:09', ',2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` bigint(20) NOT NULL,
+  `order_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_price` decimal(8,2) NOT NULL,
+  `product_image` varchar(255) NOT NULL,
+  `product_quantity` int(11) NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -35,8 +81,8 @@ CREATE TABLE `products` (
   `sale_price` float DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `image` text NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL
+  `category` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -44,13 +90,28 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `sale_price`, `quantity`, `image`, `category`, `type`) VALUES
-(1, 'iphone999', 'very massive tech can do everything', 100, 100, 20, 'iphone.jpg', 'smartphone', 'iphone'),
-(2, 'samsananag', 'very korea phone', 20, 1, 20, 'samsung', 'smartphone', 'samsung'),
-(3, 'oppo', 'oppopopopopop', 20, 1, 20, 'oppo.png', 'smartphone', 'oppo');
+(1, 'iphone200', 'best mobile with so much camera', 50000, 50, 5000, 'iphone.jpg', 'phone', 'iphone'),
+(2, 'rtx9000', 'best video card of the world', 99999, 99, 9, 'rtx.jpg', 'vga', 'nvidia'),
+(3, 'samsung galaxy', 'samsung galaxy galactus ', 100, 20, 5000, 'samsung.jpg', 'phone', 'samsung'),
+(4, 'jbl boom box', 'best speaker big bbc', 50000, 20, 5000, 'jbl.jpg', 'speaker', 'jbl'),
+(5, '25Robux gift card', 'u got 25 robux we got 5000 bath what a deal', 50000, 5000, 5000, 'robux.jpg', 'gift card', 'robux'),
+(6, 'tesla cybertruck', 'best car in the world', 88888900, 100, 5000, 'tesla.jpg', 'car', 'tesla');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -63,10 +124,16 @@ ALTER TABLE `products`
 --
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
